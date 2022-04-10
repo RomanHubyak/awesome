634029d7269fe5c72e6d7c720eb13dd1d78ca900
Roman Hubyak <roman.hubyak@empeek.com>
2022-04-10 21:53:58 +0300

    Fix.

--- Awesome/SqlHostedServer.cs
+++ Awesome/SqlHostedServer.cs
@@ -29,85 +29,90 @@ public class SqlHostedServer : IHostedService
     {
         while (true)
         {
-            using var scope = _serviceProvider.CreateScope();
+            try
+            {
+                using var scope = _serviceProvider.CreateScope();
 
-            var awesomeDbContext = scope.ServiceProvider.GetService<AwesomeDbContext>();
+                var awesomeDbContext = scope.ServiceProvider.GetService<AwesomeDbContext>();
 
-            var random = new Random();
+                var random = new Random();
 
-            var todoListId = random.Next(await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .CountAsync());
+                var todoListId = random.Next(await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .CountAsync());
 
-            await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .Where(x => x.Id > todoListId)
-                .ToListAsync();
+                await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .Where(x => x.Id > todoListId)
+                    .ToListAsync();
 
-            await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .Include(x => x.TodoItems)
-                .Where(x => x.Id == todoListId)
-                .ToListAsync();
+                await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .Include(x => x.TodoItems)
+                    .Where(x => x.Id == todoListId)
+                    .ToListAsync();
 
-            await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .Where(x => x.Status == ETodoListStatus.Planned)
-                .ToListAsync();
+                await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .Where(x => x.Status == ETodoListStatus.Planned)
+                    .ToListAsync();
 
-            await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .Where(x => x.Status == ETodoListStatus.Started)
-                .ToListAsync();
+                await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .Where(x => x.Status == ETodoListStatus.Started)
+                    .ToListAsync();
 
-            await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .Where(x => x.Status == ETodoListStatus.Completed)
-                .ToListAsync();
+                await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .Where(x => x.Status == ETodoListStatus.Completed)
+                    .ToListAsync();
 
-            await awesomeDbContext.TodoLists
-                .AsQueryable()
-                .Where(x => x.Status == ETodoListStatus.Canceled)
-                .ToListAsync();
+                await awesomeDbContext.TodoLists
+                    .AsQueryable()
+                    .Where(x => x.Status == ETodoListStatus.Canceled)
+                    .ToListAsync();
 
-            var todoItemId = random.Next(1000000);
+                var todoItemId = random.Next(1000000);
 
-            await awesomeDbContext.TodoItems
-                .AsQueryable()
-                .Select(x => new
+                await awesomeDbContext.TodoItems
+                    .AsQueryable()
+                    .Select(x => new
+                    {
+                        x.Id,
+                        x.Name,
+                        x.TodoListId,
+                        TodoListName = x.TodoList.Name,
+                    })
+                    .Where(x => x.Id > todoItemId)
+                    .OrderBy(x => x.TodoListId)
+                    .Take(random.Next(100000))
+                    .ToListAsync();
+
+                var letters = "qwertyuiopasdfghjklzxcvbnm";
+
+                awesomeDbContext.TodoLists.Add(new TodoList
                 {
-                    x.Id,
-                    x.Name,
-                    x.TodoListId,
-                    TodoListName = x.TodoList.Name,
-                })
-                .Where(x => x.Id > todoItemId)
-                .OrderBy(x => x.TodoListId)
-                .Take(random.Next(100000))
-                .ToListAsync();
+                    CreatedDate = DateTime.UtcNow,
+                    IsDeleted = random.Next(2) == 1,
+                    Name = new string(Enumerable.Repeat(0, random.Next(10)).Select(x => letters[random.Next(letters.Length)]).ToArray()),
+                    Status = (ETodoListStatus)(random.Next(4) + 1),
+                });
 
-            var letters = "qwertyuiopasdfghjklzxcvbnm";
+                awesomeDbContext.TodoItems.Add(new TodoItem
+                {
+                    CreatedDate = DateTime.UtcNow,
+                    IsDeleted = random.Next(2) == 1,
+                    Name = new string(Enumerable.Repeat(0, random.Next(10)).Select(x => letters[random.Next(letters.Length)]).ToArray()),
+                    Description = new string(Enumerable.Repeat(0, random.Next(20)).Select(x => letters[random.Next(letters.Length)]).ToArray()),
+                    Status = (ETodoItemStatus)(random.Next(4) + 1),
+                    TodoListId = todoListId,
+                });
 
-            awesomeDbContext.TodoLists.Add(new TodoList
+                await awesomeDbContext.SaveChangesAsync();
+            }
+            catch
             {
-                CreatedDate = DateTime.UtcNow,
-                IsDeleted = random.Next(2) == 1,
-                Name = new string(Enumerable.Repeat(0, random.Next(10)).Select(x => letters[random.Next(letters.Length)]).ToArray()),
-                Status = (ETodoListStatus)(random.Next(4) + 1),
-            });
-
-            awesomeDbContext.TodoItems.Add(new TodoItem
-            {
-                CreatedDate = DateTime.UtcNow,
-                IsDeleted = random.Next(2) == 1,
-                Name = new string(Enumerable.Repeat(0, random.Next(10)).Select(x => letters[random.Next(letters.Length)]).ToArray()),
-                Description = new string(Enumerable.Repeat(0, random.Next(20)).Select(x => letters[random.Next(letters.Length)]).ToArray()),
-                Status = (ETodoItemStatus)(random.Next(4) + 1),
-                TodoListId = todoListId,
-            });
-
-            await awesomeDbContext.SaveChangesAsync();
-
+            }
         }
     }
 }
